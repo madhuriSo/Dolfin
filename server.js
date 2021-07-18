c6415ab863387589ce11920bb586ff3a8392d1fd
madhuriSo <madhuri.padule@gmail.com>
2021-07-18 17:42:53 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express= require('express');
+const express= require("express");
 const app=express();
-const path=require('path');
+const path=require("path");
 
-app.use(express.static(path.join(__dirname,'dist/Dolfin/')));
+app.use(express.static(path.join(__dirname,"dist/Dolfin/")));
 
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname,'/dist/Dolfin/index.html'));
+    res.sendFile(path.join(__dirname,"/dist/Dolfin/index.html"));
     if(err){
         res.writeHead(404,{'Content-Type':'text/html'});
         return  res.end("404 Not Found :"+err);
