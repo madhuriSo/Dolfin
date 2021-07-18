daa09497787b7607e7e78824fe9203f29f514d09
madhuriSo <madhuri.padule@gmail.com>
2021-07-18 17:47:48 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express= require("express");
 const app=express();
 const path=require("path");
 
+
+app.use(express.static(__dirname + './dist/Dolfin'));
+/*
 app.use(express.static(path.join(__dirname,"dist/Dolfin/")));
-
-
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname,"/dist/Dolfin/index.html"));
     if(err){
@@ -12,7 +13,7 @@ app.get('/*',function(req,res){
         return  res.end("404 Not Found :"+err);
     }
 
-})
+})*/
 app.listen(process.env.port||8080);
 
 console.log('console listening!');
